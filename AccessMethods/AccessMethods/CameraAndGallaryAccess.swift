7caa120da17f10912f86a4f6289799fbd6769116
karpomkarpipoom <karpomkarpipoom@gmail.com>
2020-04-19 20:03:53 +0530

    Alamofire Changes and Few Updates

---
+++ AccessMethods/AccessMethods/CameraAndGallaryAccess.swift
@@ -0,0 +1,30 @@
+//
+//  CameraAndGallaryAccess.swift
+//  AccessMethods
+//
+//  Created by BALAMURUGAN on 03/03/20.
+//  Copyright © 2020 BALAMURUGAN. All rights reserved.
+//
+
+import UIKit
+
+class CameraAndGallaryAccess: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
